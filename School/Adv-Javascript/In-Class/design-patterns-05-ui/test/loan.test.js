cc2d9e4b0ed194c60102e6f14e872602ef70db65
jyee0012 <jyee12@studentmail.nait.ca>
2018-11-12 09:57:38 -0700

    testing class

---
+++ School/Adv-Javascript/In-Class/design-patterns-05-ui/test/loan.test.js
@@ -0,0 +1,51 @@
+import Loan from '../js/dist/loan';
+import {expect} from 'chai';
+
+// var expect = require('chai').expect;
+let loan = null;
+describe('Test', function () {
+    beforeEach(function () {
+        loan = new Loan();
+    });
+    describe('#Loan() constructor', function () {
+        context('when called without arugments', function () {
+
+            it('should initialize default property title', function () {
+                expect(loan.title).to.be.a('string');
+                expect(loan.title).to.equal('');
+            });
+            it('should initialize default property principal to 0.0', function() {
+                expect(loan.principal).to.be.a('number');
+                expect(loan.principal).to.equal(0);
+            });
+
+        });
+        context('when called with arguments', function () {
+            it('should initialize all desired properties', function () {
+                loan = new Loan({
+                    title: 'Title',
+                    principal: 1000,
+                    rate: 0.1,
+                    term: 1
+                });
+                expect(loan.title).to.equal('Title');
+                // TODO: assertions for remaining props
+            });
+        });
+        describe('#set()', function () {
+            it('should set the desired property on the loan', function () {
+                loan.set('term', 1);
+                expect(loan.term).to.equal(1);
+            });
+            it.skip('should trigger \'change\' event', function () {
+                // how can we test this? .. later
+            });
+        });
+        describe('#get()', function () {
+            it('should get the desired property from the loan', function () {
+                loan.term = 1
+                expect(loan.get('term')).to.equal(1);
+            });
+        });
+    });
+});
---
+++ School/Adv-Javascript/Practical3/assignment-03-start/test/loan.test.js
@@ -0,0 +1,83 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+import Loan from '../js/dist/Loan';
+
+let l = null;
+
+describe('Loan', function() {
+    beforeEach(function() {
+        l = new Loan();
+    });
+    describe("#Loan constructor", function() {
+        context('when called without arguments', function() {
+            it('should initialize default property title', function() {
+                expect(l.title).to.be.a('string');
+            });
+            it('should initialize default property principal', function() {
+                expect(l.principal).to.be.a('number');
+            });
+            it('should initialize default property rate', function() {
+                expect(l.rate).to.be.a('number');
+            });
+            it('should initialize default property term', function() {
+                expect(l.term).to.be.a('number');
+            });
+        });
+        context('when called with arguments', function() {
+            it('should initialize all desired properties', function() {
+                l = new Loan({
+                    title: 'Title',
+                    principal: 1,
+                    rate: 0.01,
+                    term: 1
+                });
+                expect(l.title).to.equal('Title');
+                expect(l.principal).to.equal(1);
+                expect(l.rate).to.equal(0.01);
+                expect(l.term).to.equal(1);
+            });
+        });
+    });
+    describe('#set()', function() {
+        it('should set the desired property on the loan', function() {
+            l.set('term', 1);
+            expect(l.term).to.equal(1);
+        });
+        it('should trigger the \'change\' event', function() {
+            // stub the trigger function to achieve isolation (don't want to actually 
+            // call the original trigger function)
+            var triggerSpy = sinon.stub(l, 'trigger');
+
+            // alternative to create true dependency separation
+            // var triggerSpy = l.trigger = sinon.spy();
+
+            // stub the trigger function on the loan
+            l.set('term', 1);
+            // ensure that the call to trigger was made with 'change' as the argument
+            expect(triggerSpy.calledWith('change')).to.equal(true);
+        });
+    });
+    describe('#get()', function() {
+        it('should get the desired property from the loan', function() {
+            l.term = 1;
+            expect(l.get('term')).to.equal(1);
+        });
+    });
+    describe('#payment()', function() {
+        it('should return the correct payment amount', function() {
+            l.principal = 1000;
+            l.term = 1;
+            l.rate = 0.05;
+            expect(l.payment()).to.be.within(85.61, 85.63);
+        });
+    });
+    // add cost() function test here ...
+    describe('#cost()', function () {
+        it('should return current cost amount based off formula', function () {
+            l.principal = 1000;
+            l.term = 1;
+            l.rate = 0.05;
+            expect(l.cost()).to.be.within(27.28, 27.30);
+        });
+    });
+});
