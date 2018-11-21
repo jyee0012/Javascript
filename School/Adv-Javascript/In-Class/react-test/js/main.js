e09d30293d9b1ae737d1c5689af9c0bed53812bf
jyee0012 <jyee12@studentmail.nait.ca>
2018-11-21 09:52:12 -0700

    moar react class

--- School/Adv-Javascript/In-Class/react-test/js/main.js
+++ School/Adv-Javascript/In-Class/react-test/js/main.js
@@ -1,2 +1,26 @@
 const domContainer = document.querySelector('.react-container');
-ReactDOM.render(<HelloWorld/>, domContainer);
+// ReactDOM.render(<HelloWorld/>, domContainer);
+const App = () => {
+    return (
+        <div>
+            <Friend friendData={friendData[0]}/>
+            <Friend friendData={friendData[1]}/>
+        </div>
+    )
+};
+const friendData = [
+    {
+        firstName:"Sume", 
+        lastName:"oneulse", 
+        email:"someonelse@gmail.com", 
+        avatar:"someone.jpg"
+    },
+    {
+        firstName:"Onne", 
+        lastName:"Uls", 
+        email:"onelse@gmail.com", 
+        avatar:"one.jpg"
+    }
+];
+
+ReactDOM.render(<App/>, domContainer);
--- School/Adv-Javascript/In-Class/react-test/js/my-component.js
+++ School/Adv-Javascript/In-Class/react-test/js/my-component.js
@@ -1,12 +1,43 @@
 'use strict';
 
 // Most the basic Reac component
-const HelloWorld = () => {
-    return ( 
-        <div>
-        <p>Hello, World </p> 
+// const HelloWorld = () => {
+//     return ( 
+//         <div>
+//         <p>Hello, World </p> 
+//         </div>
+//       )
+// }
+
+// const greetingStyle = {
+//     color: 'blue'
+// }
+const friendStyle = {
+
+}
+const friendListStyle = {
+    
+}
+// Passing simple props to the component
+// const Greeting = (props) => {
+//     return <div style={greetingStyle}>Hello {props.name}</div>
+// }
+const Friend = (props) => {
+    const {friendData} = props;
+    return (
+        <div style={friendStyle}>
+            <img src={friendData.avatar}/>
+            <div>{friendData.firstName} {friendData.lastName}</div>
+            <div>{friendData.email}</div>
         </div>
-      )
+    );
+}
 
-
-    }
+// const App = () => {
+//     return (
+//         <div>
+//             <Greeting name="Sume onelse"/>
+//             <Greeting name="Sam Sumone"/>
+//         </div>
+//     );
+// }
